b07225a6d0aed8621b15c34419246f3fa27d9633
Miha Uranič <miha.uranic08@gmail.com>
2026-09-14 06:42:48 +0000

    api_commit2

--- OPR_exs/uvodAPI.py
+++ OPR_exs/uvodAPI.py
@@ -1,3 +1,5 @@
+import requests
+"""
 # dictionary
 
 dictionary = {
@@ -25,3 +27,20 @@ import requests
 base_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=rain_sum&hourly=rain"
 call = requests.get(base_url).json()
 print(call["daily"]["rain_sum"])
+"""
+#Exercises
+#https://hackmd.io/@lukac/api1
+#1.
+#base_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1"
+#call = requests.get(base_url).json()
+#print(call["current"]["temperature_2m"])
+
+#2.
+base_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin"
+call = requests.get(base_url).json()
+#print(call["daily"]["temperature_2m_min"], call["daily"]["temperature_2m_max"])
+
+#3.
+maximum = call["daily"]["temperature_2m_max"].index(max(call["daily"]["temperature_2m_max"]))
+minimum = call["daily"]["temperature_2m_min"].index(min(call["daily"]["temperature_2m_min"]))
+print(call["daily"]["time"][maximum], call["daily"]["time"][minimum])
